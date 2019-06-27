c6c4aaf047cd4cbd26ae615826c2a6009ffc0816
Mihir Lad <mihirlad55@gmail.com>
2019-06-27 01:15:33 -0400

    Bind programs to tags

--- config.def.h
+++ config.def.h
@@ -35,9 +35,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class        instance    title       tags mask     isfloating   monitor */
+    { "Station",    NULL,       NULL,       1 << 7,       0,           -1 },
+    { "Slack",      NULL,       NULL,       1 << 9,       0,           -1 },
+    { "discord",    NULL,       NULL,       1 << 9,       0,           -1 },
+    { "spotify",    NULL,       NULL,       1 << 6,       0,           -1 },
+    { "Mailspring", NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
